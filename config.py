bdf884e14ab2df684484baa03327d08b914b7110
OstAnd21 <ostashevskyi.andrii@gmail.com>
2026-09-25 13:42:49 +0300

    Add retry settings to configuration

--- config.py
+++ config.py
@@ -13,6 +13,20 @@ class Settings:
         "BASE_URL",
         "https://jsonplaceholder.typicode.com"
     )
+    
+    RETRY_STATUS = int(
+        os.getenv(
+            "RETRY_STATUS",
+            "3"
+        )
+    )
+
+    RETRY_BACKOFF_FACTOR = float(
+        os.getenv(
+            "RETRY_BACKOFF_FACTOR",
+            "1"
+        )
+    )
 
     ALLOWED_ENVS = {"dev", "test", "prod"}
     def validate(self):
